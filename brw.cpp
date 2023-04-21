bebdee1c6587590add8d188402d8f0c6d08aa627
Max Hofer <max.jakob.hofer@gmail.com>
2023-04-21 17:23:12 +0200

    Added u_field class

--- brw.cpp
+++ brw.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <limits>
 
+///////////////////////////////////////////////// BRW /////////////////////////////////////////////////
+
 std::mt19937 BRW::engine(BRW::seed);
 
 void BRW::evolve_one_step(unsigned long det_thr)
@@ -46,6 +48,8 @@ std::string d_to_str(double x, int precision)
     return ss.str();
 }
 
+///////////////////////////////////////////////// progress_monitor /////////////////////////////////////////////////
+
 void progress_monitor::reset()
 {
     starting_time = std::chrono::steady_clock::now();
@@ -81,6 +85,215 @@ std::ostream& operator<<(std::ostream& out, const progress_monitor& pm)
     return out;
 }
 
+///////////////////////////////////////////////// u_field /////////////////////////////////////////////////
+
+u_field::u_field(unsigned lambda_pµ, unsigned saving_period) : _lambda_pµ(lambda_pµ), _lambda(lambda_pµ / 1e6), _saving_period(saving_period)
+{
+    compute_velocity();
+    if (lambda_pµ > 1000000) throw std::range_error("Out of range: lambda_pµ must be within [1,1e6].");
+}
+
+u_field::real_t u_field::operator()(sidx x, idx t) const
+{
+    sidx st = t;
+    if (t > std::numeric_limits<sidx>::max()) throw std::domain_error("t is to large to be converted to a signed number.");
+    if (x < -st) return 1.;
+    else if (x > st) return 0.;
+    return u(t, (t + x + 1)/2); //if 2|(t+x), the +1 is rounded away, otherwise u(x,t)=u(x+1,t)
+}
+
+void u_field::fill_checkpoints(idx T, double tol)
+{
+    progress_monitor pm;
+    idx output_period = T > 100 ? T / 100 : 1;
+
+    idx t_max = T - T%_saving_period;
+    idx t_min = (u_map.size() == 0) ? 0 : u_map.rbegin()->first + 1;
+    double tol0 = std::exp(-2*_gamma0*std::sqrt(T));
+    for (idx t = t_min; t != t_max + 1; ++t)
+    {
+        fill_row(t, tol0, tol, true);
+        if ((t - 1) % _saving_period != 0 && t > _saving_period) u_map.erase(t - 1);
+        if ((t + 1) % output_period == 0)
+        {
+            pm.add_datapoint(t / (double) T);
+            std::cout << "\x1B[2J\x1B[H" << "Filling u for lambda = " << d_to_str(_lambda, 6) << " for T = " << T << "\n" << pm << std::endl;
+            std::cout << "Memory usage of u: " << estimate_memory() << " byte" << std::endl;
+        }
+    }
+}
+
+void u_field::fill_between(idx T1, idx T2, double tol, bool overwrite)
+{
+    double tol0 = std::exp(-2*_gamma0*std::sqrt(T2));
+    for (idx t = T1; t != T2 + 1; ++t) fill_row(t, tol0, tol, overwrite);
+}
+
+std::ostream& operator<<(std::ostream& out, const u_field& u)
+{
+    out << u._lambda_pµ << " " << u._saving_period << std::endl;
+    for (auto& l : u.lower_approx_bound) out << l << " ";
+    out << std::endl;
+    for (auto& up : u.upper_approx_bound) out << up << " ";
+    out << std::endl;
+    for (auto& key_value: u.u_map)
+    {
+        out << key_value.first << " ";
+        for (auto& val : key_value.second)
+        {
+            out << val << " ";
+        }
+        out << std::endl;
+    }
+    return out;
+}
+
+std::istream& operator>>(std::istream& in, u_field& u)
+{
+    double lam_pµ;
+    unsigned sav_per;
+    
+    if (!(in >> lam_pµ)) throw std::runtime_error("Couldn't read in lambda.");
+    u._lambda_pµ = lam_pµ;
+    u._lambda = lam_pµ / 1e6;
+    
+    if (!(in >> sav_per)) throw std::runtime_error("Couldn't read in saving_period.");
+    u._saving_period = sav_per;
+        
+    std::string line;
+    if (!(std::getline(in, line))) throw std::runtime_error("Couldn't read in lower_approx_bound.");
+    std::istringstream iss(line);
+    u_field::idx i;
+    u.lower_approx_bound.clear();
+    while (iss >> i) u.lower_approx_bound.push_back(i);
+    
+    if (!(std::getline(in, line))) throw std::runtime_error("Couldn't read in upper_approx_bound.");
+    iss.str(line);
+    iss.clear();
+    u.upper_approx_bound.clear();
+    while (iss >> i) u.upper_approx_bound.push_back(i);
+
+    if(!std::getline(in, line)) throw std::runtime_error("Now values of u to read in.");
+    do
+    {
+        iss.str(line);
+        iss.clear();
+        u_field::idx t;
+        iss >> t;
+        u_field::real_t value;
+        while (iss >> value) u.u_map[t].push_back(value);
+    }
+    while (std::getline(in, line));
+
+    return in;
+}
+
+unsigned long u_field::estimate_memory(bool rough) const
+{
+    if (u_map.size() == 0 || u_map.rbegin()->second.size() == 0) return 0;
+    else if (rough) return (sizeof(idx) + sizeof(real_t) * u_map.rbegin()->second.size()) * u_map.size();
+    unsigned long num_vec_entries = 0;
+    for (const auto& kd : u_map) num_vec_entries += kd.second.size();
+    return sizeof(idx) * u_map.size() + num_vec_entries;
+}
+
+void u_field::print(std::ostream& out, unsigned digits, idx window_size)
+{
+    for (sidx t = 0; t != window_size; ++t)
+    {
+        for (sidx x = - (sidx) window_size; x != window_size + 1; ++x)
+        {
+            int i = (t+x)/2;
+            out << std::setw(digits + 3) << d_to_str(u(t,i), digits);
+        }
+        out << "\n" << std::endl;
+    }
+}
+
+void u_field::compute_velocity()
+{
+    if (_lambda_pµ == 0)
+    {
+        _velocity = 0;
+        return;
+    }
+    else if (_lambda_pµ == 1000000U)
+    {
+        _velocity = 1;
+        return;
+    }
+    real_t gamma1 = sqrt(2.*log(1.+_lambda));
+    std::function<real_t(real_t)> f = [this](real_t g){ return g*std::tanh(g)-std::log((1.+_lambda)*std::cosh(g)); };
+    std::function<real_t(real_t)> f_prime = [this](real_t g){ return g*(1-std::pow(std::tanh(g),2)); };
+    unsigned int i = 0;
+    do
+    {
+        _gamma0 = gamma1;
+        gamma1 = _gamma0 - f(_gamma0) / f_prime(_gamma0);
+        ++i;
+    }
+    while (std::abs(gamma1 - _gamma0)/_gamma0 > 1e-14 && i != 100);
+    _velocity = std::log((1.+_lambda)*std::cosh(gamma1)) / gamma1;
+    _gamma0 = gamma1;
+    if (i == 100) throw std::domain_error("Newton's method did not reach the required degree of precision within 100 steps.");
+}
+
+void u_field::fill_row(idx t, double tol0, double tol1, bool overwrite)
+{
+    if (!overwrite && u_map.count(t) != 0) return;
+    else if (t == 0)
+    {
+        if (lower_approx_bound.size() == 0) lower_approx_bound.push_back(0);
+        if (upper_approx_bound.size() == 0) upper_approx_bound.push_back(0);
+        if (u_map.count(t) == 0) u_map[0] = {1.};
+    }
+    else if (u_map.count(t-1) != 1) throw std::runtime_error("The (t-1)-th row has not been computed yet! t = " + std::to_string(t));
+    else
+    {
+        real_t new_u;
+        real_t ut1_i;
+        real_t ut1_i1 = 1.;
+        for (idx i = lower_approx_bound[t-1]; i != t + 1; ++i)
+        {
+            ut1_i = u(t-1, i);
+            new_u = (1. + _lambda) / 2. * (ut1_i1 + ut1_i) - _lambda / 2. * (ut1_i1 * ut1_i1 + ut1_i * ut1_i);
+            ut1_i1 = ut1_i; //the future previous value is the present new value
+            if (lower_approx_bound.size() == t && 1. - new_u > tol1)
+            {
+                lower_approx_bound.push_back(i); //The second condition assures that it is only set once.
+            }
+            if (upper_approx_bound.size() == t && new_u <= tol0) //set the upper bound once 
+            {
+                upper_approx_bound.push_back(i != 0 ? i - 1 : 0);
+                return; //Once the upper approximation bound is set, the row is filled.
+            }
+            if (lower_approx_bound.size() > t) u_map[t].push_back(new_u); //save u_new iff the lower bound of the exact region has been set.
+        }
+        //If the program reaches this point, the upper_approximation bound has not been set yet and must be set to its maximum value of t.
+        if (upper_approx_bound.size() < lower_approx_bound.size()) upper_approx_bound.push_back(t);
+    }
+}
+
+u_field::real_t u_field::u(idx t, idx i) const
+{
+    if (t == 0 && i == 0 || lower_approx_bound.size() > t && i < lower_approx_bound[t]) return 1.;
+    else if (upper_approx_bound.size() > t && i > upper_approx_bound[t]) return 0.;
+    else
+    {
+        auto it = u_map.find(t);
+        if (it != u_map.end()) return it->second.at(i - lower_approx_bound[t]);
+        auto ut1_i = u(t-1,i);
+        if (i != 0)
+        {
+            auto ut1_i1 = u(t-1,i-1);
+            return (1. + _lambda) / 2. * (ut1_i1 + ut1_i) - _lambda / 2. * (ut1_i1 * ut1_i1 + ut1_i * ut1_i);
+        }
+        else return 1.;
+    }
+}
+
+///////////////////////////////////////////////// model /////////////////////////////////////////////////
+
 void model::compute_velocity()
 {
     if (lambda_pm == 0)
@@ -113,6 +326,7 @@ void model::compute_velocity()
 model::model(long T, long X, unsigned saving_period, double tol) : _X(X), _T(T), _I((T+X)/2), _J((T-X)/2), pm_r(), pm_u(),
             _lambda(lambda_pm / 1000.), logw_minus(std::log((1.-_lambda)/2.)), logw_plus(std::log((1.+_lambda)/2.)), approx_tol(tol), saving_period(saving_period)
 {
+    output_period = _I > 100 ? _I / 100 : 1;
     compute_velocity();
 }
 
@@ -122,6 +336,7 @@ model::model(long T, unsigned saving_period, double tol) : model(T, 0, saving_pe
     _X = bareX%2 ? bareX + 1 : bareX;
     _I = (T+_X)/2;
     _J = (T-_X)/2;
+    output_period = _I > 100 ? _I / 100 : 1;
 }
 
 #ifdef PRINT_W_MEMORY
@@ -141,15 +356,27 @@ void model::print_w_map()
 }
 #endif
 
+long model::u_size()
+{
+    if (u_map.size() == 0 || u_map.rbegin()->second.size() == 0) return 0;
+    else return (sizeof(idx) + sizeof(real_t) * u_map.rbegin()->second.size()) * u_map.size();
+}
+
+long model::r_size()
+{
+    if (logr_map.size() == 0 || logr_map.rbegin()->second.size() == 0) return 0;
+    else return (sizeof(idx) + sizeof(real_t) * logr_map.rbegin()->second.size()) * logr_map.size();
+}
+
 void model::fill_u_row(idx i) //fill the ith row assuming that the (i-1)th one has already been filled (if i > 0).
 {
-    if (i == 0)
+    if (u_map.count(i) != 0) return; //throw std::runtime_error("The i-th row is already in memory! i = "+i);
+    else if (i == 0)
     {
         if (lower_approx_bound.size() == 0) lower_approx_bound.push_back(0);
         if (upper_approx_bound.size() == 0) upper_approx_bound.push_back(0);
         if (u_map.count(i) == 0) u_map[0] = {1.};
     }
-    else if (u_map.count(i) != 0) throw std::runtime_error("The i-th row is already in memory! i = "+i);
     else if (u_map.count(i-1) != 1) throw std::runtime_error("The (i-1)-th row has not been computed yet! i = " + i);
     else
     {
@@ -163,7 +390,10 @@ void model::fill_u_row(idx i) //fill the ith row assuming that the (i-1)th one h
             ui1_j = u(i-1, j);
             new_u = (1. + _lambda) / 2. * (ui_j1 + ui1_j) - _lambda / 2. * (ui_j1 * ui_j1 + ui1_j * ui1_j);
             ui_j1 = new_u; //the future previous value is the present new value
-            if (new_u > approx_tol && lower_approx_bound.size() == i) lower_approx_bound.push_back(j); //the second condition assures that it is only set once.
+            if (lower_approx_bound.size() == i && new_u > std::exp(-2 * gamma0 * std::sqrt((double) _T)))
+            {
+                lower_approx_bound.push_back(j); //the second condition assures that it is only set once.
+            }
             if (1. - new_u <= approx_tol && upper_approx_bound.size() == i) //set the upper bound once 
             {
                 upper_approx_bound.push_back(j != 0 ? j - 1 : 0);
@@ -178,7 +408,6 @@ void model::fill_u_row(idx i) //fill the ith row assuming that the (i-1)th one h
 
 void model::fill_u()
 {
-    idx output_period = _I > 100 ? _I / 100 : 1;
     for (idx i = 0; i <= _I; ++i)
     {
         fill_u_row(i);
@@ -187,24 +416,8 @@ void model::fill_u()
         {
             pm_u.add_datapoint(i / (double) _I);
             std::cout << "\x1B[2J\x1B[H" << "Filling u for lambda = " << d_to_str(_lambda, 3) << " for T = " << _T << "\n" << pm_u << std::endl;
-            std::cout << "Memory usage of u: " << (sizeof(idx) + sizeof(real_t)) * u_map.size() << " byte\n"
-                << "Memory usage of r: " << (2 * sizeof(idx) + sizeof(real_t)) * logr_map.size() << " byte" << std::endl;
-        }
-    }
-}
-
-void model::fill_logr()
-{
-    idx output_period = _I > 100 ? _I / 100 : 1;
-    for (unsigned i = _I-_I%saving_period; i <= _I; i -= saving_period) //when i == 0, the next subtraction will make it i=max_unsigned - saving_period > I
-    {
-        logr(i, 0);
-        if ( (i + 1) % output_period == 0)
-        {
-            pm_r.add_datapoint((_I - (double) i) / _I);
-            std::cout << "\x1B[2J\x1B[H" << "Filling r for lambda = " << d_to_str(_lambda, 3) << " for T = " << _T << "\n" << pm_r << std::endl;
-            std::cout << "Memory usage of u: " << (sizeof(idx) + sizeof(real_t)) * u_map.size() << " byte\n"
-                    << "Memory usage of r: " << (2 * sizeof(idx) + sizeof(real_t)) * logr_map.size() << " byte" << std::endl;
+            std::cout << "Memory usage of u: " << u_size() << " byte\n"
+                << "Memory usage of r: " << r_size() << " byte" << std::endl;
         }
     }
 }
@@ -272,36 +485,64 @@ model::real_t model::logq(idx i, idx j)
     else return 0.;
 }
 
+void model::fill_logr_row(idx i) //fill the ith row assuming that the (i+1)th one has already been filled (if i < I).
+{
+    if (logr_map.count(i) != 0) throw std::runtime_error("The i-th row is already in memory! i = " + i);
+    else if (i == _I)
+    {
+        logr_map[_I].push_front(0.);
+        for (idx j = _J-1; j != ((idx) -1); --j)
+        {
+            logr_map[i].push_front(logr_map.at(_I).front() + logw(i,j));
+        }
+    }
+    else if (logr_map.count(i+1) != 1) throw std::runtime_error("The (i+1)-th row has not been computed yet! i = " + i);
+    else
+    {
+        logr_map[i].push_front(logr_map.at(i+1).at(_J) + logw(i,_J));
+        for (idx j = _J-1; j != ((idx) -1); --j)
+        {
+            real_t logri1_j = logr_map.at(i+1).at(j);
+            logr_map[i].push_front(logri1_j + std::log(w(i,j)*(1. + std::exp(logr_map.at(i).front()-logri1_j))));
+        }
+    }
+}
+
+void model::fill_logr()
+{
+    for (idx i = _I; i != ((idx) -1); --i) //when i == 0, the next subtraction will make it i=max_unsigned - saving_period > I
+    {
+        fill_logr_row(i);
+        if ((i + 1) % output_period == 0)
+        {
+            pm_r.add_datapoint((_I - (double) i) / _I);
+            std::cout << "\x1B[2J\x1B[H" << "Filling r for lambda = " << d_to_str(_lambda, 3) << " for T = " << _T << "\n" << pm_r << std::endl;
+            std::cout << "Memory usage of u: " << u_size() << " byte\n"
+                << "Memory usage of r: " << r_size() << " byte" << std::endl;
+        }
+        else if (i > 50) //in this case i+1 is no checkpoint and can be deleted
+        {
+            logr_map.erase(i+1);
+        }
+    }
+}
+
 model::real_t model::logr(idx i, idx j)
 {
-    bool checkpoint = !(i%saving_period); //if i is a multiple of saving_period, its congruence class is 0=false, so checkpoint=true
-    // if (checkpoint && i + saving_period <= _I) model::logr(i+saving_period, j); //if checkpoint, make sure that previous checkpoint gets evaluated first
-    // else if (i + saving_period <= _I) model::logr(i + saving_period-i%saving_period, j); //if not checkpoint, make sure that previous checkpoint gets evaluated first
-    real_t xdeviation = (1.+velocity)*i+(velocity-1.)*j;
     if (i != _I)
     {
-        auto it = logr_map.find({i,j});
-        if (it != logr_map.end()) return it->second;
+        auto it = logr_map.find(i);
+        if (it != logr_map.end()) return it->second.at(j);
         else
         {
-            if (!checkpoint) //if this i-value is not a checkpoint, clean up the values at i-saving_period and i+saving_period
-            {
-                logr_map.erase({i-saving_period, j});
-                logr_map.erase({i+saving_period, j});
-            }
             auto logri1_j = logr(i+1,j);
-            if (j != _J) return logr_map[{i,j}] = logri1_j + std::log(w(i,j)*(1. + std::exp(logr(i,j+1)-logri1_j)));
-            else return logr_map[{i,j}] = logri1_j + logw(i,j);
+            if (j != _J) return logri1_j + std::log(w(i,j)*(1. + std::exp(logr(i,j+1)-logri1_j)));
+            else return logri1_j + logw(i,j);
         }
     }
     else if (j != _J)
     {
-        if (!checkpoint) //if this i-value is not a checkpoint, clean up the values at i-saving_period and i+saving_period
-        {
-            logr_map.erase({i-saving_period, j});
-            logr_map.erase({i+saving_period, j});
-        }
-        return logr_map[{i,j}] = logr(i,j+1) + std::log(w(i,j));
+        return logr(i,j+1) + logw(i,j);
     }
     else return 0.;
 }
--- brw.hpp
+++ brw.hpp
@@ -53,6 +53,59 @@ class progress_monitor
         friend std::ostream& operator<<(std::ostream& out, const progress_monitor& pm);
 };
 
+class u_field
+{
+    public:
+        using sidx = int; //signed index type
+        using idx = unsigned int;
+        using real_t = long double;
+        //This class holds the values of u in the scaling region for a given value of lambda. The saving_period is the distance between two checkpoints.
+        u_field(unsigned lambda_pµ, unsigned saving_period = 1);
+        //Returns u(x,t) assuming step sizes dx == dt == 1
+        real_t operator()(sidx x, idx t) const;
+        /*Fills in all checkpoints <= T starting from the last row that is saved. If tol = 0., all non-zero values of u are computed.
+        Values for which 1-u < tol or u < exp(-2*gamma0*sqrt(T2)) are not saved.*/
+        void fill_checkpoints(idx T, double tol = 0.);
+        /*
+        Fills all rows from inclusively T1 to inclusively T2.
+        If overwrite == true, already filled rows including checkpoints will be deleted first.
+        If overwrite == false, rows that are already filled are skipped.
+        Values for which 1-u < tol or u < exp(-2*gamma0*sqrt(T2)) are not saved.
+        */
+        void fill_between(idx T1, idx T2, double tol = 0., bool overwrite = false);
+        /*Writes the object to "out". The first row contains lambda and the saving_period.
+        The second and third rows contain all values of lower_approx_bound and upper_approx_bound.
+        The remaining lines start with the time index and afterwards the entries of u at this time index.
+        Only the values that are saved are outputted.*/
+        friend std::ostream& operator<<(std::ostream& out, const u_field& u);
+        //Reads in the object from "in", following the format outputted by operator<<.
+        friend std::istream& operator>>(std::istream& in, u_field& u);
+        //Output the approximate memory used to save u. If rough == true, it is assumed that all rows contain the same number of elements as the latest row.
+        unsigned long estimate_memory(bool rough = true) const;
+        double lambda() const { return _lambda; }
+        unsigned saving_period() const { return _saving_period; }
+        double velocity() const { return _velocity; }
+        double gamma0() const { return _gamma0; }
+        void print(std::ostream& out, unsigned digits = 3, idx window_size = 20);
+    private:
+        double _lambda;
+        unsigned long _lambda_pµ;
+        unsigned _saving_period;
+        double _velocity;
+        double _gamma0;
+        std::map<idx, std::vector<real_t>> u_map;
+        std::vector<idx> lower_approx_bound;
+        std::vector<idx> upper_approx_bound;
+        void compute_velocity();
+        /*Fill in row t assuming that row t-1 has already been filled.
+        If overwrite == true, the potentially filled row is erased first.
+        If overwrite == false, but the row is already filled, nothing happens.
+        Values for which 1-u < tol1 or u < tol0 are not saved.*/
+        void fill_row(idx t, double tol0 = 0., double tol1 = 0., bool overwrite = false);
+        //Returns u given (t,i)-coordinates.
+        real_t u(idx t, idx i) const;
+};
+
 class model
 {
     public:
@@ -66,9 +119,10 @@ class model
         const real_t logw_minus;
         inline static real_t velocity;
         inline static real_t gamma0;
+        unsigned output_period;
         inline static std::map<idx, std::vector<real_t>> u_map;
         inline static std::map<std::pair<idx, idx>, real_t> logq_map;
-        std::map<std::pair<idx, idx>, real_t> logr_map;
+        std::map<idx, std::deque<real_t>> logr_map;
         void compute_velocity();
         #ifdef PRINT_W_MEMORY
         void print_w_map();
@@ -79,6 +133,9 @@ class model
         progress_monitor pm_u;
         progress_monitor pm_r;
         void fill_u_row(idx i);
+        void fill_logr_row(idx i);
+        long u_size();
+        long r_size();
     public:
         inline static std::vector<idx> lower_approx_bound;
         inline static std::vector<idx> upper_approx_bound;
@@ -88,6 +145,7 @@ class model
         model(long T, unsigned saving_period, double tol = 0.);
         void fill_u();
         void fill_logr();
+        idx m_t(idx i); // gives the index j such that [i,j] corresponds to the coordinates (t,m_t)
         real_t u(idx i, idx j);
         real_t w(idx i, idx j);
         real_t logw(idx i, idx j);
@@ -97,6 +155,8 @@ class model
         real_t pplus(idx i, idx j);
         inline const real_t& lambda() const { return _lambda; }
         inline const real_t& v() const { return velocity; }
+        inline const real_t& gamma() const { return gamma0; }
+        inline const unsigned out_period() const { return output_period; }
         inline long X() const { return _X; }
         inline long T() const { return _T; }
         inline idx I() const { return _I; }
